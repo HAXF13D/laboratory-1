dc6585207a49642af5ef69535f5f560f28fd0f94
HAXF13D <vovik0312@gmail.com>
2021-09-08 15:48:49 +0300

    Maximum

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 a = int(input("a = "))
 b = int(input("b = "))
-if b == 0:
-    print("Error you can't divide by 0")
-else:
-    print(f"a // b = {a // b}")
+print(f"Максимум из двух чисел = {max(a, b)}")
